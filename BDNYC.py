ed9293bed476ac638646d2a587dc99534f3d05c5
alejocosmico <alejo.nunez@gmail.com>
2012-06-15 09:43:41 -0400

    Fixed use of matchUNum on BDNYCData methods; fixed appending snr to request in giveSpectrum.

--- BDNYC.py
+++ BDNYC.py
@@ -3,18 +3,22 @@
 """
 BDNYC.py
 
-Last updated by: Dan 2/10/2012
+Last updated by: Dan 6/14/2012
 
 The code underlying our BDNYC Python Database, based at AMNH.
-
 """
 
-import sys, os, pylab
+import sys
+import os
+import pylab
 import numpy as np
+import pdb
 
 class Target:
-    """An object containing data for a specific target object to be put into
-    the brown dwarf database. This flavor produces one extra level of nesting."""
+    """
+    An object containing data for a specific target object to be put into
+    the brown dwarf database. This flavor produces one extra level of nesting.
+    """
     
     def __init__(self,name,unum,ra,dec,sptype,opt,nir,mir,standard=None):
         self.name = name
@@ -28,21 +32,27 @@ class Target:
         self.standard = standard
     
 
+
 class BDNYCData:
-    """Class to hold all of the target objects and methods for Brown Dwarf
-    analysis."""
+    """
+    Class to hold all of the target objects and methods for Brown Dwarf analysis.
+    """
     
     def __init__(self):
         self.targets = []
     
     def addTarget(self, targetObj):
-        """Adds a new target object to the database."""
+        """
+        Adds a new target object to the database.
+        """
         self.targets.append(targetObj)
     
     def matchUNum(self, unum, array=False, index=True):
-        """Matches a U# to a target so the code knows which object you are 
+        """
+        Matches a U# to a target so the code knows which object you are 
         referring to. This function returns the array of U#s, the index of 
-        the target object in question, or both, in that order."""
+        the target object in question, or both, in that order.
+        """
         
         uNumbers = np.array([], dtype='S6')
         for target in self.targets:
@@ -62,13 +72,13 @@ class BDNYCData:
             return
     
     def plotOrders(self, unum, instr, date):
-        ind = matchUNum(unum)
+        ind = self.matchUNum(unum)
         for i in self.targets[ind].nir['high'][instr][date].keys():
             pylab.plot(self.targets[ind].nir['high'][instr][date][i]['wl'], \
             self.targets[ind].nir['high'][instr][date][i]['flux'])
     
     def overPlot(self, unum, med_instr, h_instr, dateM, dateH):
-        ind = matchUNum(unum)
+        ind = self.matchUNum(unum)
         for i in self.targets[ind].nir['high'][h_instr][dateH].keys():
             pylab.plot(self.targets[ind].nir['high'][h_instr][dateH][i]['wl'], \
             self.targets[ind].nir['high'][h_instr][dateH][i]['flux'], 'b')
@@ -77,9 +87,11 @@ class BDNYCData:
             self.targets[ind].nir['med'][med_instr][dateM][j]['flux'], 'r')
     
     def dateList(self, obsType, res, surv_instr):
-        """Sort through the dates and list all targets with observations on said 
+        """
+        Sort through the dates and list all targets with observations on said 
         date. obsType is opt, nir, or mir. res is what type of data (i.e. low, med,
-        high). surv_instr is the survey or instrument observed with."""
+        high). surv_instr is the survey or instrument observed with.
+        """
         
         dates = {}
         if obsType != 'nir' and obsType != 'mir' and obsType != 'opt':
@@ -144,47 +156,47 @@ class BDNYCData:
         File.close()
         return
     
-    def giveSpectrum(self, unum=None, otype=None, res=None, instr=None, date=None, \
-                     snr=False, order=None, Filter=None):
-        """Function to output a spectrum in a numpy array as [wl, flux, snr(optional)]
+    def giveSpectrum(self, unum=None, otype=None, res=None, instr=None, date=None, snr=False, order=None, Filter=None):
+        """
+        Function to output a spectrum in a numpy array as [wl, flux, snr(optional)]
         The necessary info about the spectrum must be provided as keyword arguments.
-         
+        
         The kwargs are:
-        unum = U number associated with the target whose spectrum you want
-        type = observation type, i.e. opt, nir, or mir
-        res = the resolution of the spectrum, i.e. low, med, or high
-        instr = the instrument or survey, e.g. NIRSPEC, Spex, Phoenix
-        date = the observation date, given in format YYYYmonDD, e.g. 2009jan23
-        snr = whether or not to include the snr array. This is True or False
-        order = if high resolution desired, then you must specify the order
-        filter = if medium resolution is desired, then you must specify the filter."""
+        unum   = U number associated with the target whose spectrum you want
+        otype  = observation type, i.e. opt, nir, or mir
+        res    = the resolution of the spectrum, i.e. low, med, or high
+        instr  = the instrument or survey, e.g. NIRSPEC, Spex, Phoenix
+        date   = the observation date, given in format YYYYmonDD, e.g. 2009jan23
+        snr    = whether or not to include the snr array. This is True or False
+        order  = if high resolution desired, then you must specify the order
+        filter = if medium resolution is desired, then you must specify the filter.
+        """
         
-        
-        specInd = matchUNum(unum)
+        specInd = self.matchUNum(unum)
         if otype=='opt':
             
             if res=='high':
                 specArr = np.array([self.targets[specInd].opt['high'][instr][date]\
                 [order]['wl'], self.targets[specInd].opt['high'][instr][date][order]['flux']])
                 if snr:
-                    specArr = np.append(specArr, self.targets[specInd].opt['high'][instr]\
-                    [date][order]['snr'])
+                    specArr = np.append(specArr, [self.targets[specInd].opt['high'][instr]\
+                    [date][order]['snr'], axis=0)
                 return specArr
             
             if res=='med':
                 specArr = np.array([self.targets[specInd].opt['med'][instr][date][Filter]\
                 ['wl'], self.targets[specInd].opt['med'][instr][date][Filter]['flux']])
                 if snr:
-                    specArr = np.append(specArr, self.targets[specInd].opt['med'][instr]\
-                    [date][Filter]['snr'])
+                    specArr = np.append(specArr, [self.targets[specInd].opt['med'][instr]\
+                    [date][Filter]['snr']], axis=0)
                 return specArr
             
             if res=='low':
                 specArr = np.array([self.targets[specInd].opt['low'][instr][date]\
                 ['wl'], self.targets[specInd].opt['low'][instr][date]['flux']])
                 if snr:
-                    specArr = np.append(specArr, self.targets[specInd].opt['low'][instr]\
-                    [date]['snr'])
+                    specArr = np.append(specArr, [self.targets[specInd].opt['low'][instr]\
+                    [date]['snr']], axis=0)
                 return specArr
         
         if otype=='nir':
@@ -193,24 +205,24 @@ class BDNYCData:
                 specArr = np.array([self.targets[specInd].nir['high'][instr][date]\
                 [order]['wl'], self.targets[specInd].nir['high'][instr][date][order]['flux']])
                 if snr:
-                    specArr = np.append(specArr, self.targets[specInd].nir['high'][instr]\
-                    [date][order]['snr'])
+                    specArr = np.append(specArr, [self.targets[specInd].nir['high'][instr]\
+                    [date][order]['snr']], axis=0)
                 return specArr
             
             if res=='med':
                 specArr = np.array([self.targets[specInd].nir['med'][instr][date][Filter]\
                 ['wl'], self.targets[specInd].nir['med'][instr][date][Filter]['flux']])
                 if snr:
-                    specArr = np.append(specArr, self.targets[specInd].nir['med'][instr]\
-                    [date][Filter]['snr'])
+                    specArr = np.append(specArr, [self.targets[specInd].nir['med'][instr]\
+                    [date][Filter]['snr']], axis=0)
                 return specArr
             
             if res=='low':
                 specArr = np.array([self.targets[specInd].nir['low'][instr][date]\
                 ['wl'], self.targets[specInd].nir['low'][instr][date]['flux']])
                 if snr:
-                    specArr = np.append(specArr, self.targets[specInd].nir['low'][instr]\
-                    [date]['snr'])
+                    specArr = np.append(specArr, [self.targets[specInd].nir['low'][instr]\
+                    [date]['snr']], axis=0)
                 return specArr
         
         if otype=='mir':
@@ -219,27 +231,23 @@ class BDNYCData:
                 specArr = np.array([self.targets[specInd].mir['high'][instr][date]\
                 [order]['wl'], self.targets[specInd].mir['high'][instr][date][order]['flux']])
                 if snr:
-                    specArr = np.append(specArr, self.targets[specInd].mir['high'][instr]\
-                    [date][order]['snr'])
+                    specArr = np.append(specArr, [self.targets[specInd].mir['high'][instr]\
+                    [date][order]['snr']], axis=0)
                 return specArr
             
             if res=='med':
                 specArr = np.array([self.targets[specInd].mir['med'][instr][date][Filter]\
                 ['wl'], self.targets[specInd].mir['med'][instr][date][Filter]['flux']])
                 if snr:
-                    specArr = np.append(specArr, self.targets[specInd].mir['med'][instr]\
-                    [date][Filter]['snr'])
+                    specArr = np.append(specArr, [self.targets[specInd].mir['med'][instr]\
+                    [date][Filter]['snr']], axis=0)
                 return specArr
             
             if res=='low':
                 specArr = np.array([self.targets[specInd].mir['low'][instr][date]\
                 ['wl'], self.targets[specInd].mir['low'][instr][date]['flux']])
                 if snr:
-                    specArr = np.append(specArr, self.targets[specInd].mir['low'][instr]\
-                    [date]['snr'])
+                    specArr = np.append(specArr, [self.targets[specInd].mir['low'][instr]\
+                    [date]['snr']], axis=0)
                 return specArr
     
-
-
-
-
